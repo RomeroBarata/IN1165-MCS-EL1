d357206b22859f1f957119b8a9b74f7b5221c19d
RomeroBarata <romero.barata@gmail.com>
2016-09-02 22:58:52 -0300

    Update bagging functions

--- R/bagging-functions.R
+++ R/bagging-functions.R
@@ -16,59 +16,60 @@ generateBootstrapSamples <- function(m, b){
   replicate(b, sample(m, size = m, replace = TRUE), simplify = FALSE)
 }
 
-#' Compute the Out-of-Bag examples.
+#' Generate an ensemble of trees through the bagging algorithm.
 #' 
-#' \code{oobExamples} returns a matrix of logicals identifying for each example 
-#' which bootstrap sample they did not participate.
+#' \code{bagging} generates a tree ensemble using the bagging strategy, where 
+#' each tree in the ensemble is trained with a boostrap sample from the 
+#' original data set.
 #' 
-#' The resulting matrix can be used to compute the out-of-bag error of a 
-#' bagging procedure.
+#' The base decision tree is the one available in the \code{rpart} package.
 #' 
-#' @inheritParams generateBootstrapSamples
-#' @param bootstrap_samples A list where each element contains the indices of 
-#'  a bootstrap sample.
-#' @return A matrix of logicals where each row represents an example and each
-#'  column represents a bootstrap sample. The i,j-th entry is \code{TRUE} if 
-#'  the i-th example is not present in the j-th bootstrap sample, and 
-#'  \code{FALSE} otherwise.
+#' @param data A data frame containing the predictors and the outcome. The 
+#'  outcome must be a binary factor and the last column.
+#' @param L Number of trees in the ensemble.
+#' @param cores Number of cores for parallel processing. If running on Windows 
+#'  \code{cores} must be left as 1.
+#' @return A list containing the trained trees, the trees' predictions, and 
+#'  the out-of-bag error of the ensemble.
 
-oobExamples <- function(m, bootstrap_samples){
-  vapply(bootstrap_samples, function(x) !(1:m %in% x), logical(m))
-}
-
-bagging <- function(data, L = 100, oob_error = FALSE, cores = 1){
+bagging <- function(data, L = 100, cores = 1){
   # In order to use the formula interface in the rpart function
   names(data)[ncol(data)] <- "Class"
   
+  # Generate the bootstrap samples
   m <- nrow(data)  # Number of examples
-  bss <- generateBootstrapSamples(m, L)
+  bootstrap_samples <- generateBootstrapSamples(m, L)
   
-  trainDecisionTree <- function(x){
-    rpart::rpart(Class ~ ., data = data, subset = x, 
+  trainDecisionTree <- function(bootstrap_sample){
+    rpart::rpart(Class ~ ., data = data, subset = bootstrap_sample, 
                  method = "class", parms = list(split = "information"))
   }
-  bagging_models <- parallel::mclapply(bss, trainDecisionTree, mc.cores = cores)
+  bagging_models <- parallel::mclapply(bootstrap_samples, 
+                                       trainDecisionTree, mc.cores = cores)
   
-  if (oob_error == TRUE) 
-    preds <- oobPreds(bagging_models, subset(data, select = -Class), bss, cores)
-  
-  error <- 1 - mean(subset(data, select = Class) == preds)
-  
-  list(bagging_models = bagging_models, oob_error = error)
-}
-
-oobPreds <- function(bagging_models, data_preds, bootstrap_samples, cores){
-  m <- nrow(data_preds)  # Number of examples
-  oob_matrix <- oobExamples(m, bootstrap_samples)
-  oob_models_list <- apply(oob_matrix, 1, function(x) which(x))
-  
-  majorityVoting <- function(i){
-    example <- data_preds[i, ]
-    models_idx <- oob_models_list[[i]]
-    preds <-sapply(bagging_models[models_idx], 
-                   function(model) predict(model, example, type = "class"))
-    names(which.max(table(preds)))
+  makePreds <- function(model, x, bootstrap_sample){
+    preds <- rep(NA, nrow(x))
+    preds[-bootstrap_sample] <- predict(model, x[-bootstrap_sample, ], type = "class")
+    preds
   }
-  
-  unlist(parallel::mclapply(1:m, majorityVoting, mc.cores = cores))
+  preds <- parallel::mcMap(makePreds, 
+                           bagging_models, 
+                           list(subset(data, select = -Class)), 
+                           bootstrap_samples, 
+                           mc.cores = cores)
+  lvls <- levels(data$Class)
+  preds <- lapply(preds, function(x) ifelse(x == 1, lvls[1],
+                                            ifelse(x == 2, lvls[2], NA)))
+  preds <- matrix(unlist(preds, use.names = FALSE), ncol = L)
+  colnames(preds) <- paste("Tree", 1:L)
+
+  majorityVote <- function(x){
+    x <- x[!is.na(x)]
+    if (length(x) == 0) return(NA)
+    names(which.max(table(x)))
+  }
+  oob_preds <- apply(preds, 1, majorityVote)
+  oob_error <- 1 - mean(data$Class == oob_preds, na.rm = TRUE)
+  list(bagging_models = bagging_models, models_predictions = preds, 
+       oob_error = oob_error)
 }
